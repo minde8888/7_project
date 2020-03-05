b318aedef861b5ce4881b0609a8ac2c86ddb2611
minde8888 <58484005+minde8888@users.noreply.github.com>
2020-03-05 11:54:17 +0200

    ciklai

--- js/ciklai.js
+++ js/ciklai.js
@@ -1,4 +1,28 @@
+console.log('--------------------------------dienos');
 
+const dienos = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+let counter = 0;
+let  menuo = 0;
+let  diena= 0;
+for(let m=0; m<12; m++){
+    for(let d=0; d<=dienos[m]; d++){
+        counter++;
+        menuo = m+1;
+        diena =d+1;
+        console.log('('+counter+')' + menuo + ': ' + diena);
+    }
+}
+
+console.log('--------------------------------LAIKAS');
+
+
+for(let h=0; h<12; h++){
+    for(let m=0; m<60; m++){
+        for(let s=0; s<60; s++){
+            console.log(h + ':' + m + ':' + s);
+        }
+    }
+}
 
 console.log('-------------------------------------------------');
 
@@ -7,13 +31,13 @@ let sum2=0;
 let sum3=0;
 
 for (let i = 0; i <= 11; i++) {
-    if(i%3==0 && i!=0){
+    if(i%3==0){
         sum1=sum1+1;
     }
-    if(i%5==0 && i!=0){
+    if(i%5==0){
         sum2=sum2+1;
     }
-    if(i%7==0 && i!=0){
+    if(i%7==0){
         sum3=sum3+1;
     }
 }
@@ -21,6 +45,21 @@ console.log('dalinasi is 3 -',sum1);
 console.log('dalinasi is 5 -',sum2); 
 console.log('dalinasi is 7 -',sum3); 
 
+console.log('-------------------------------------------------');
+
+const from = 0;
+const till = 11;
+const divider = 3;
+let cou = 0;
+
+for(let i = from; i<=till; i++){
+   // console.log(i+'->'+(i % divider))
+    if(i % divider === 0){
+        cou++;
+    }
+}
+console.log('Skaičių intervale tarp '+from+' ir '+till+', besidalinančių be liekanos iš '+divider+' yra  '+cou+' vienetai.'
+   )
 
 console.log('-------------------------------------------------');
 
@@ -37,9 +76,19 @@ let    s = '';
      
 let count = 0;
 
-    for(let i=n.length-1; i>0; i-- ){
+    /*for(let i=n.length-1; i>=0; i-- ){
        s +=n[i]; 
     }
-    console.log(s);
+    console.log(s);*/
+
+   /* for(let i=0; i<=n.length; i++ ){
+        s +=n[n.length-i-1]; 
+     }
+     console.log(s);*/
+
+     for(let i=0; i<n.length; i++ ){
+        s = n[i] + s; 
+     }
+     console.log(s);
 
 console.log('-------------------------------------------------');
